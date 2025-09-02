469764ffab05787d11dd6c24d1e2ba70a055a2c9
bogdantrefler <bogdantrefler27@gmail.com>
2025-09-02 17:33:32 +0300

    Add files via upload

---
+++ Labarator.py
@@ -0,0 +1,12 @@
+age = 17.5
+name = "Bogdan"
+is_student = True
+favorite_color = "black"
+city = "Lutsk"
+fruit = ["Banana"]
+max_speed = 100
+car_brand = {"BMW","Audi"}
+pi_number = "3,14159"
+coutry = "Ukraine"
+
+print(age,name,is_student,favorite_color,city,fruit,max_speed,car_brand,pi_number,coutry)
