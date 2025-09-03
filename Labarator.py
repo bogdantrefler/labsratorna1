fcd2e9463116622302eccfcbf4daaf39eb3beb2b
bogdantrefler <bogdantrefler27@gmail.com>
2025-09-03 11:01:53 +0300

    Update Labarator.py

--- Labarator.py
+++ Labarator.py
@@ -9,5 +9,15 @@ car_brand = {"BMW","Audi"}
 pi_number = 3,14159
 coutry = "Ukraine"
 data = (age,name,is_student,favorite_color,city,fruit,max_speed,car_brand,pi_number,coutry)
-print(type(data))
 print(data)
+print(type(data))
+print(type(age))
+print(type(name))
+print(type(is_student))
+print(type(favorite_color))
+print(type(city))
+print(type(fruit))
+print(type(max_speed))
+print(type(car_brand))
+print(type(pi_number))
+print(type(coutry))
